e7cead762de458595b21bcf5dc88c94c759e3ece
srima karekal <srima589.karekal@gmail.com>
2016-05-23 11:37:43 +0530

    commit

---
+++ src/Encap.java
@@ -0,0 +1,27 @@
+
+public class Encap {
+  private int empid;
+  private String empname;
+  private int empage;
+  
+  public int getempid(){
+	  return empid;
+  }
+  public String getempname(){
+	  return empname;
+  }
+  public int getempage(){
+	  return empage;
+  }
+  public void setempid(int newid){
+	  empid=newid;
+  }
+  public void setempname(String newname){
+	  empname=newname;
+  }
+  public void setempage(int age){
+	  empage=age;
+  }
+  
+  
+}
---
+++ src/EncapDemo.java
@@ -0,0 +1,16 @@
+
+public class EncapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encap encap=new Encap();
+encap.setempid(112345);
+encap.setempname("mallika");
+encap.setempage(34);
+System.out.println("employee id:"+encap.getempid());
+System.out.println("empoyee name:"+encap.getempname());
+System.out.println("employee age:"+encap.getempage());
+
+	}
+
+}
